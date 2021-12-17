fd70f731281c630001bba11cf48b62610a8d6d4d
Chiro Liang <chiro2001@163.com>
2021-12-17 21:58:23 +0800

    trying difftest...

--- tests/cpu-tests/assembly.mk
+++ tests/cpu-tests/assembly.mk
@@ -2,7 +2,7 @@ ARCH_SPLIT = $(subst -, ,$(ARCH))
 ISA        = $(word 1,$(ARCH_SPLIT))
 PLATFORM   = $(word 2,$(ARCH_SPLIT))
 
-ALL_ASM = $(basename $(notdir $(shell find assembly/$(ISA)/. -name "*.S")))
+ASM = $(basename $(notdir $(shell find assembly/$(ISA)/. -name "*.S")))
 ASM_ARG = $(word 2,$(subst -, ,$(MAKECMDGOALS)))
 ifeq ($(ASM_ARG), reg)
 ASM_ARG = run
@@ -11,25 +11,23 @@ endif
 include pass.mk
 
 ifdef PASS
-ALL_ASM = $(PASS)
+ASM = $(PASS)
 endif
 
-asm-all: $(addprefix ASM.mk., $(ALL_ASM))
-	@echo "All assembly tests:" $(ALL_ASM) $(ASM_ARG)
+asm-all: $(addprefix ASM.mk., $(ASM))
+	@echo "All assembly tests:" $(ASM)
 
 asm: asm-all
 
 asm-gdb: asm
 
 asm-run: asm-all
-	@cat $(RESULT)
-	@rm $(RESULT)
 
 asm-reg: asm-run
 
-$(ALL_ASM): %: ASM.mk.%
+$(ASM): %: ASM.mk.%
 
-ASM.mk.%: assembly/$(ISA)/%.S latest
+ASM.mk.%: assembly/$(ISA)/*/%.S latest
 	@/bin/echo -e "NAME = $*\nSRCS = $<\nNO_LIB = true\ninclude $${AM_HOME}/Makefile" > $@
 	@if make -s -f $@ ARCH=$(ARCH) $(ASM_ARG); then \
 		printf "[%14s] $(COLOR_GREEN)PASS!$(COLOR_NONE)\n" $* >> $(RESULT); \
@@ -37,4 +35,6 @@ ASM.mk.%: assembly/$(ISA)/%.S latest
 		printf "[%14s] $(COLOR_RED)FAIL!$(COLOR_NONE)\n" $* >> $(RESULT); \
 	fi
 	-@rm ASM.mk.*
+	@cat $(RESULT)
+	@rm $(RESULT)
 	
