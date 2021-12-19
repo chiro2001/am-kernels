a939163f3e07c340b0325c3a69bcef0b2e196254
Chiro Liang <chiro2001@163.com>
2021-12-19 22:39:03 +0800

    -> add *printf

--- tests/cpu-tests/tests/hello-str.c
+++ tests/cpu-tests/tests/hello-str.c
@@ -1,19 +1,16 @@
 #include "trap.h"
 
-// char buf[128];
+char buf[128];
 
 int main() {
-	// sprintf(buf, "%s", "Hello world!\n");
-	// // check(strcmp(buf, "Hello world!\n") == 0);
+	sprintf(buf, "%s", "Hello world!\n");
+	check(strcmp(buf, "Hello world!\n") == 0);
 
 	// sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
-	// // check(strcmp(buf, "1 + 1 = 2\n") == 0);
+	// check(strcmp(buf, "1 + 1 = 2\n") == 0);
 
 	// sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
-	// // check(strcmp(buf, "2 + 10 = 12\n") == 0);
-
-	printf("#");
-	// putch('#');
+	// check(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	return 0;
 }
